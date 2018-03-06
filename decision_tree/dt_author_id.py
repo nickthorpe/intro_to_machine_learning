e7e62e1d180968f80c74adf256d1c407f73cafbd
Nick Thorpe <nickthorpe13@gmail.com>
2018-03-05 21:38:14 -0500

    finished mini proj 3

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,8 +19,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print len(features_train[0])
 
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+clf = clf.fit(features_train,labels_train)
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(labels_test,pred)
 
 #########################################################
 ### your code goes here ###
